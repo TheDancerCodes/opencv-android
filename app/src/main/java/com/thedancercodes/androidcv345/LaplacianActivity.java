bc3b10bc6f8790c8b4ea1e2a403892cfc18b4e45
Roger Taracha <rojtaracha@gmail.com>
2020-05-27 17:38:03 +0300

    Project cleanup.

--- app/src/main/java/com/thedancercodes/androidcv345/LaplacianActivity.java
+++ app/src/main/java/com/thedancercodes/androidcv345/LaplacianActivity.java
@@ -1,6 +1,5 @@
 package com.thedancercodes.androidcv345;
 
-import android.content.Context;
 import android.content.Intent;
 import android.content.res.ColorStateList;
 import android.graphics.Bitmap;
@@ -9,7 +8,6 @@ import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
-import android.os.PersistableBundle;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
@@ -22,7 +20,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 import androidx.core.content.FileProvider;
 
-import com.google.android.material.card.MaterialCardView;
 import com.google.android.material.chip.Chip;
 
 import org.jetbrains.annotations.NotNull;
@@ -36,10 +33,8 @@ import org.opencv.core.Size;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 
-import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
-import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -50,10 +45,6 @@ public class LaplacianActivity extends AppCompatActivity {
     private ImageView mImageView;
     private TextView mTextView;
     private Chip mChip;
-    private MaterialCardView materialCardView;
-    private TextView textCpuArchitecture;
-
-    private static final int REQUEST_IMAGE_CAPTURE = 101;
 
     private static final String TAG = "LaplacianActivity";
     public static final int PICK_IMAGE_REQUEST_CODE = 1001;
@@ -72,52 +63,6 @@ public class LaplacianActivity extends AppCompatActivity {
 
     private String [] permissions = {"android.permission.WRITE_EXTERNAL_STORAGE", "android.permission.ACCESS_FINE_LOCATION", "android.permission.READ_PHONE_STATE", "android.permission.SYSTEM_ALERT_WINDOW","android.permission.CAMERA"};
 
-//    private void opencvProcess() {
-//        BitmapFactory.Options options = new BitmapFactory.Options();
-//        options.inDither = true;
-//        options.inPreferredConfig = Bitmap.Config.ARGB_8888;
-//        Bitmap image = decodeSampledBitmapFromFile(picFilePath, options, 2000, 2000);
-//        int l = CvType.CV_8UC1; //8-bit grey scale image
-//        Mat matImage = new Mat();
-//        Utils.bitmapToMat(image, matImage);
-//        Mat matImageGrey = new Mat();
-//        Imgproc.cvtColor(matImage, matImageGrey, Imgproc.COLOR_BGR2GRAY);
-//
-//        Bitmap destImage;
-//        destImage = Bitmap.createBitmap(image);
-//        Mat dst2 = new Mat();
-//        Utils.bitmapToMat(destImage, dst2);
-//        Mat laplacianImage = new Mat();
-//        dst2.convertTo(laplacianImage, l);
-//        Imgproc.Laplacian(matImageGrey, laplacianImage, CvType.CV_8U);
-//        Mat laplacianImage8bit = new Mat();
-//        laplacianImage.convertTo(laplacianImage8bit, l);
-//
-//        Bitmap bmp = Bitmap.createBitmap(laplacianImage8bit.cols(), laplacianImage8bit.rows(), Bitmap.Config.ARGB_8888);
-//        Utils.matToBitmap(laplacianImage8bit, bmp);
-//        int[] pixels = new int[bmp.getHeight() * bmp.getWidth()];
-//        bmp.getPixels(pixels, 0, bmp.getWidth(), 0, 0, bmp.getWidth(), bmp.getHeight()); // bmp为轮廓图
-//
-//        int maxLap = -16777216; // 16m
-//        for (int pixel : pixels) {
-//            if (pixel > maxLap)
-//                maxLap = pixel;
-//        }
-//
-//        int soglia = -6118750;
-//        if (maxLap <= soglia) {
-//            System.out.println("is blur image");
-//        }
-//        soglia += 6118750;
-//        maxLap += 6118750;
-//        LogUtil.log("图片位置=" + picFilePath
-//                + "\nimage.w=" + image.getWidth() + ", image.h=" + image.getHeight()
-//                + "\nmaxLap= " + maxLap + "(清晰范围:0~6118750)"
-//                + "\n" + Html.fromHtml("<font color='#eb5151'><b>" + (maxLap <= soglia ? "模糊" : "清晰") + "</b></font>"));
-//        opencvEnd = true;
-//        isBlur = maxLap <= soglia;
-//    }
-
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -126,8 +71,7 @@ public class LaplacianActivity extends AppCompatActivity {
         mImageView = findViewById(R.id.imageView);
         mTextView = findViewById(R.id.textView);
         mChip = findViewById(R.id.chip);
-        materialCardView = findViewById(R.id.card);
-        textCpuArchitecture = findViewById(R.id.textCpuArchitecture);
+        TextView textCpuArchitecture = findViewById(R.id.textCpuArchitecture);
 
         textCpuArchitecture.setText(getString(R.string.cpu_architecture, System.getProperty("os.arch")));
 
@@ -139,11 +83,6 @@ public class LaplacianActivity extends AppCompatActivity {
     }
 
     public void takePicture(View view) {
-//        Intent imageTakeIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-//
-//        if (imageTakeIntent.resolveActivity(getPackageManager()) != null) {
-//            startActivityForResult(imageTakeIntent, REQUEST_IMAGE_CAPTURE);
-//        }
 
         // Capture Image & save it into the file path
         Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
@@ -194,22 +133,15 @@ public class LaplacianActivity extends AppCompatActivity {
         return imageFile;
     }
 
-//     Receive the result from the other application
+    // Receive the result from the camera application
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
 
         if (requestCode==IMAGE_REQUEST && resultCode==RESULT_OK) {
-//            Bundle extras = data.getExtras();
-//            Bitmap imageBitmap = (Bitmap) extras.get("data");
 
             Bitmap bitmap = BitmapFactory.decodeFile(currentImagePath);
 
             mImageView.setImageBitmap(bitmap);
-
-            //getImageUri(this, imageBitmap);
-
-            //String imagePath = getImageUri(this, imageBitmap).getPath();
-            //Toast.makeText(this, currentImagePath, Toast.LENGTH_SHORT).show();
 
             // Covert ImagePath to Matrix
             Mat img = Imgcodecs.imread(currentImagePath, Imgcodecs.IMREAD_COLOR);
@@ -219,56 +151,28 @@ public class LaplacianActivity extends AppCompatActivity {
                 Log.d(TAG, "Error opening image");
                 Toast.makeText(this, "Error opening image", Toast.LENGTH_SHORT).show();
             } else {
-                lap_test(img);
+                laplacianFilter(img);
             }
-
         }
-
     }
 
-//    public Uri getImageUri(Context inContext, Bitmap inImage) {
-//        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
-//        inImage.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
-//        String path = MediaStore.Images.Media.insertImage(inContext.getContentResolver(), inImage, "Title", null);
-//        return Uri.parse(path);
-//
-//    }
-
-    public void lap_test(Mat img) {
+    public void laplacianFilter(Mat img) {
         // Declare the variables we are going to use
-        Mat src, src_gray = new Mat(), dst = new Mat();
+        Mat src_gray = new Mat();
+        Mat lap = new Mat();
         int kernel_size = 3;
         int scale = 1;
         int delta = 0;
-        int ddepth = CvType.CV_16S;
-
-//        String imageName = Environment.getExternalStorageDirectory() + "/Download/bokeh-336478_1280.jpg";
-//        String imageName = Environment.getExternalStorageDirectory() + "/Download/5D8A8192.jpg";
-
-//        src = Imgcodecs.imread(imageName, Imgcodecs.IMREAD_COLOR); // Load an image
-
-        // Check if image is loaded fine
-//        if (src.empty()) {
-//            Log.d("lap_test", "Error opening image");
-//        }
+        int ddepth = CvType.CV_64F;
 
         // Reduce noise by blurring with a Gaussian filter ( kernel size = 3 )
         Imgproc.GaussianBlur(img, img, new Size(3, 3), 0, 0, Core.BORDER_DEFAULT);
 
-        // // Convert the image to grayscale
+        // Convert the image to grayscale
         Imgproc.cvtColor(img, src_gray, Imgproc.COLOR_RGBA2GRAY);
 
-//        Mat abs_dst = new Mat();
-//
-//        Imgproc.Laplacian( src_gray, dst, ddepth, kernel_size, scale, delta, Core.BORDER_DEFAULT);
-//
-//        // converting back to CV_8U
-//        Core.convertScaleAbs( dst, abs_dst);
-//
-//        Log.d("lap_test", String.valueOf(abs_dst));
-
-        Mat lap = new Mat();
-        Imgproc.Laplacian(src_gray, lap, CvType.CV_64F);
+        // Leveraging the Laplacian filter
+        Imgproc.Laplacian(src_gray, lap, ddepth);
 
         MatOfDouble mean = new MatOfDouble();
         MatOfDouble dev = new MatOfDouble();
@@ -276,25 +180,12 @@ public class LaplacianActivity extends AppCompatActivity {
         Core.meanStdDev(lap, mean, dev);
         double value = dev.get(0, 0)[0];
 
-        Log.d("lap_test", String.valueOf(value));
-
-        // Old Threshold
-//        Toast.makeText(this, String.valueOf(value), Toast.LENGTH_LONG).show();
-
-//        DecimalFormat("0.00").format(Math.pow(std.get(0, 0)[0], 2.0)).toDouble()
-
-        // Updated Threshold
-        //String value2 = (new DecimalFormat("0.00")).format(Math.pow(dev.get(0, 0)[0], 2.0D));
-        int value2 = (int) Math.pow(dev.get(0, 0)[0], 2.0D);
-
-//        Toast.makeText(this, String.valueOf(value2), Toast.LENGTH_LONG).show();
-//        Log.d("lap_test", String.valueOf(value2));
+        Log.d("laplacianFilter", String.valueOf(value));
 
         mChip.setVisibility(View.VISIBLE);
 
         if (value < 3) {
             Toast.makeText(this, BLURRED_IMAGE, Toast.LENGTH_LONG).show();
-//            mTextView.setText(String.valueOf(value));
             mTextView.setText(getString(R.string.result_from_opencv, BLURRED_IMAGE, String.valueOf(BLUR_THRESHOLD), String.valueOf(value)));
             mChip.setChipBackgroundColor(ColorStateList.valueOf(ContextCompat.getColor(this, R.color.blurred_image)));
             mChip.setText(BLURRED_IMAGE);
@@ -328,7 +219,6 @@ public class LaplacianActivity extends AppCompatActivity {
 
                 switch (status) {
                     case BaseLoaderCallback.SUCCESS:
-//                        lap_test();
                         break;
                     default:
                         super.onManagerConnected(status);
@@ -345,34 +235,4 @@ public class LaplacianActivity extends AppCompatActivity {
             baseLoaderCallback.onManagerConnected(baseLoaderCallback.SUCCESS);
         }
     }
-
-    private void lap2() {
-
-        String image = Environment.getExternalStorageDirectory() + "/Download/bokeh-336478_1280.jpg";
-
-        Mat gray = new Mat();
-//        Imgproc.cvtColor(image, gray, Imgproc.COLOR_BGR2GRAY);
-
-        Mat lap = new Mat();
-        Imgproc.Laplacian(gray, lap, CvType.CV_64F);
-
-        MatOfDouble mean = new MatOfDouble();
-        MatOfDouble dev = new MatOfDouble();
-
-        Core.meanStdDev(lap, mean, dev);
-        double value = dev.get(0,0)[0];
-
-        Log.d("Lap Val", String.valueOf(value));
-    }
-
-
-
-    public static Mat LaplacianContrast(Mat img) {
-        Mat laplacian = new Mat();
-        Imgproc.Laplacian(img, laplacian, img.depth());
-        //Imgproc.Laplacian(img, laplacian, img.depth(), 3, 1, 0);
-        Core.convertScaleAbs(laplacian, laplacian);
-        return laplacian;
-    }
-
 }
